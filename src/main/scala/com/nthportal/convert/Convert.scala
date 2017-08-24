b7d1a4e641c9934c29a0c3b5e3143f42a457207e
NthPortal <nthportal@gmail.com>
2017-08-24 08:25:49 -0400

    Specialize methods

--- src/main/scala/com/nthportal/convert/Convert.scala
+++ src/main/scala/com/nthportal/convert/Convert.scala
@@ -5,13 +5,16 @@ import scala.reflect.ClassTag
 import scala.util.control.ControlThrowable
 
 sealed trait Convert {
+
+  import Convert.specTypes
+
   type Result[T]
 
-  def conversion[T](res: => T): Result[T]
+  def conversion[@specialized(specTypes) T](res: => T): Result[T]
 
   def fail(ex: => Exception): Nothing
 
-  def unwrap[T](result: Result[T]): T
+  def unwrap[@specialized(specTypes) T](result: Result[T]): T
 
   @inline
   final def require(requirement: Boolean): Unit = {
@@ -23,11 +26,11 @@ sealed trait Convert {
     if (!requirement) fail(new IllegalArgumentException("requirement failed: " + message))
   }
 
-  final def wrapException[E <: Exception : ClassTag, T](res: => T): T = {
+  final def wrapException[E <: Exception : ClassTag, @specialized(specTypes) T](res: => T): T = {
     wrapException(implicitly[ClassTag[E]].runtimeClass.isInstance(_))(res)
   }
 
-  final def wrapException[T](matches: Exception => Boolean)(res: => T): T = {
+  final def wrapException[@specialized(specTypes) T](matches: Exception => Boolean)(res: => T): T = {
     try {
       res
     } catch {
@@ -37,21 +40,22 @@ sealed trait Convert {
 }
 
 object Convert {
+  private val specTypes = new Specializable.Group((Byte, Short, Int, Long, Float, Double, Boolean))
 
   object Valid extends Convert {
     override type Result[T] = T
 
-    override def conversion[T](res: => T): T = res
+    override def conversion[@specialized(specTypes) T](res: => T): T = res
 
     override def fail(ex: => Exception): Nothing = throw ex
 
-    override def unwrap[T](result: T): T = result
+    override def unwrap[@specialized(specTypes) T](result: T): T = result
   }
 
   object Any extends Convert {
     override type Result[T] = Option[T]
 
-    override def conversion[T](res: => T): Option[T] = {
+    override def conversion[@specialized(specTypes) T](res: => T): Option[T] = {
       try {
         Some(res)
       } catch {
@@ -61,7 +65,7 @@ object Convert {
 
     override def fail(ex: => Exception): Nothing = throw FailControl
 
-    override def unwrap[T](result: Option[T]): T = result match {
+    override def unwrap[@specialized(specTypes) T](result: Option[T]): T = result match {
       case Some(t) => t
       case None => throw FailControl
     }
